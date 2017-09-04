2dfb059c772c1d0954220cbba5b5f55354761d99
fanhuajun <357724264@qq.com>
2017-09-04 10:09:57 +0800

    s

--- MySQL/firefighting/firefighting-数据检查.sql
+++ MySQL/firefighting/firefighting-数据检查.sql
@@ -1,8 +1,8 @@
 -- 实时数据情况--
-SELECT rtu.`third_device_id`,rtu.`app_id`, rtu.`project_code`, vanp.`PROJECT_NAME`, COUNT(sigd.`data_id`) 实时数据, MAX(sigd.`acquisition_time`)
-FROM rtu_info rtu, signal_info sig,
- signal_data_real sigd,vanke_project vanp
-   WHERE sig.`rtu_id` = rtu.`rtu_id`
-   AND sig.`signal_id` = sigd.`signal_info_id`
-   AND rtu.`project_code` = vanp.`PROJECT_ID`
-   GROUP BY rtu.`project_code`;
+SELECT rtu.`app_id`, rtu.`project_code`, vanp.`PROJECT_NAME`, MAX(sigd.`acquisition_time`), rtu.`third_device_id`, COUNT(sigd.`data_id`) 实时数据
+FROM rtu_info rtu, signal_info sig, signal_data_real sigd,vanke_project vanp
+   WHERE sig.`rtu_id` = rtu.`rtu_id` AND sig.`signal_id` = sigd.`signal_info_id`
+   AND rtu.`project_code` = vanp.`PROJECT_ID` GROUP BY rtu.`project_code`;
+
+-- 实时数据总数--
+SELECT COUNT(*) 数据总数 FROM signal_data_real;
