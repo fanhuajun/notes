aba584006111c0da018f956c2821c0e9f14d2959
Jfanhuajun <357724264@qq.com>
2018-08-31 21:38:43 +0800

    zabbix_java

--- jenkins/linux-soft/zabbix-source-java.sh
+++ jenkins/linux-soft/zabbix-source-java.sh
@@ -10,7 +10,8 @@ LISTEN_PORT=10052      #监听端口
 START_POLLERS=5        # 开启的工作线程数（必须大于等于后面zabbix_server.conf文件的StartJavaPollers参数）
 EOF
 
-/usr/local/zabbix/sbin/zabbix_java/shutdown.sh
+/usr/local/zabbix/sbin/zabbix_java/startup.sh
+/usr/local/zabbix/sbin/zabbix_server
 
 cat << EOF >>/usr/local/zabbix/etc/zabbix_server.conf
 JavaGateway=127.0.0.1                     # JavaGateway 服务器地址，zabbix_server与zabbix_java_gateway在同一台主机
