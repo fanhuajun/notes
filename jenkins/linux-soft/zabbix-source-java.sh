1b2c6d9202da82dc46833b48e10b33d14e6013f1
Jfanhuajun <357724264@qq.com>
2018-08-31 21:29:09 +0800

    9200

---
+++ jenkins/linux-soft/zabbix-source-java.sh
@@ -0,0 +1,19 @@
+
+./configure --prefix=/usr/local/zabbix --enable-java
+make install
+
+#zabbix_java  https://www.cnblogs.com/Eivll0m/p/5446311.html
+sed -i '/^#/d;/^$/d' /usr/local/zabbix/sbin/zabbix_java/settings.sh
+cat << EOF >>/usr/local/zabbix/sbin/zabbix_java/settings.sh
+LISTEN_IP="0.0.0.0"       #监听地址
+LISTEN_PORT=10052      #监听端口
+START_POLLERS=5        # 开启的工作线程数（必须大于等于后面zabbix_server.conf文件的StartJavaPollers参数）
+EOF
+
+/usr/local/zabbix/sbin/zabbix_java/shutdown.sh
+
+cat << EOF >>/usr/local/zabbix/etc/zabbix_server.conf
+JavaGateway=127.0.0.1                     # JavaGateway 服务器地址，zabbix_server与zabbix_java_gateway在同一台主机
+JavaGatewayPort=10052                    #端口
+StartJavaPollers=5
+EOF
--- jenkins/linux-soft/zabbix-source.sh
+++ jenkins/linux-soft/zabbix-source.sh
@@ -87,15 +87,9 @@ $DB['USER']                             = 'zabbix';
 $DB['PASSWORD']                 = '123456';
 // Schema name. Used for IBM DB2 and PostgreSQL.
 $DB['SCHEMA']                   = '';
+
 $ZBX_SERVER                             = 'localhost';
 $ZBX_SERVER_PORT                = '10051';
 $ZBX_SERVER_NAME                = '';
 $IMAGE_FORMAT_DEFAULT   = IMAGE_FORMAT_PNG;
-// Elasticsearch url (can be string if same url is used for all types).
-$HISTORY['url']   = [
-                'uint' => 'http://localhost:9200',
-                'text' => 'http://localhost:9200'
-];
-// Value types stored in Elasticsearch.
-$HISTORY['types'] = ['uint', 'text'];
 EOF
